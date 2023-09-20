b99ef204aa5c3328dab3867ffd46c55cee516a04
rikoteki <boiledshrimps0109@gmail.com>
2023-09-20 15:22:51 +0900

    update

--- src/routes/blogs/+server.ts
+++ src/routes/blogs/+server.ts
@@ -3,9 +3,10 @@ import { json } from '@sveltejs/kit';
 import fs from "fs";
 
 export function GET() {
-    const dirPath = "src/contents/"
-    const files = fs.readdirSync(dirPath, { withFileTypes: true })
-      .filter(dirent => dirent.isFile() && dirent.name.endsWith(".md"));
-    return json(files);
+    //const dirPath = "src/contents/"
+    //const files = fs.readdirSync(dirPath, { withFileTypes: true })
+    //  .filter(dirent => dirent.isFile() && dirent.name.endsWith(".md"));
+    //return json(files);
+    return json("test");
 }
 
