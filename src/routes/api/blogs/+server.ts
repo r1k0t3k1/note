f5da8974b12e28e814c311046cfae6a31c3edb1b
rikoteki <boiledshrimps0109@gmail.com>
2023-09-20 16:16:47 +0900

    update

--- src/routes/api/blogs/+server.ts
+++ src/routes/api/blogs/+server.ts
@@ -2,11 +2,12 @@ import { error } from '@sveltejs/kit';
 import { json } from '@sveltejs/kit';
 import fs from "fs";
 
+export const prerender = true;
+
 export function GET() {
-    //const dirPath = "src/contents/"
-    //const files = fs.readdirSync(dirPath, { withFileTypes: true })
-    //  .filter(dirent => dirent.isFile() && dirent.name.endsWith(".md"));
-    //return json(files);
-    return json({test:"test"});
+    const dirPath = "src/contents/"
+    const files = fs.readdirSync(dirPath, { withFileTypes: true })
+      .filter(dirent => dirent.isFile() && dirent.name.endsWith(".md"));
+    return json(files);
 }
 
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,3 +1,6 @@
+<script lang="ts">
+</script>
+
 <h1>
   This page is "blog".
 </h1>
