80270b1d821eddea16ddf2c66e06b35cf130efce
rikoteki <boiledshrimps0109@gmail.com>
2023-09-20 14:19:22 +0900

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import "carbon-components-svelte/css/g90.css";
+  import { base } from '$app/paths';
+</script>
+
+<h1>
+  Hello and welcome to my site!
+</h1>
+
+<a href="{base}/">home</a>
+<!--
+<a href="{base}/about">about me</a>
+<a href="{base}/blog">my blog</a>
+-->
