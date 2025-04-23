64f2d9b29d14b244670d8ec04e627f75bb14465b
r1k0t3k1 <57973603+r1k0t3k1@users.noreply.github.com>
2025-04-23 20:50:34 +0900

    Update ogp.ts

--- src/lib/ogp.ts
+++ src/lib/ogp.ts
@@ -12,6 +12,7 @@ export async function generateOgpImage(postId: string) {
 
   const browser = await puppeteer.launch({
       headless: "new",
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
   const page = await browser.newPage();
   await page.setViewport({
